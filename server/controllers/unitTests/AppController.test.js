53245b7fc91736b5f27cd3d209cc9b97beeba32a
Megan Goulet <megan.elizabeth.goulet@gmail.com>
2023-04-25 21:15:46 -0400

    Added final unit tests

--- server/controllers/unitTests/AppController.test.js
+++ server/controllers/unitTests/AppController.test.js
@@ -5,7 +5,15 @@ const AppController = require("../AppController");
  */
 test('AppController RateMyChance Test', async () => {
     value = await AppController.getMyChance();
-    expect(value.chance).toBeLessThan(1);
+    expect(value).toBeLessThan(1);
+});
+
+/**
+ * Ensures the AppController can find a college.
+ */
+test('AppController Get College Test', async () => {
+    value = await AppController.getCollege(1);
+    expect(value.College_ID).toBe(1);
 });
 
 /**
--- server/controllers/unitTests/DataController.test.js
+++ server/controllers/unitTests/DataController.test.js
@@ -4,6 +4,30 @@ const DataController = require('../DataController')
  * Ensures the DataController can find a college.
  */
 test('DataController Get College Test', async () => {
-    value = await DataController.getCollege(1);
+    value = await DataController.findCollege(1);
     expect(value.College_ID).toBe(1);
+});
+
+/**
+ * Ensures the DataController can find a list of colleges from MongoDB.
+ */
+test('DataController Get Colleges Test', async () => {
+    value = await DataController.findColleges("Rensselaer Polytechnic Institute");
+    expect(value.colleges.length).toBe(1);
+});
+
+/**
+ * Ensures the DataController can find a course.
+ */
+test('DataController Get Course Test', async () => {
+    value = await DataController.getCourse(1);
+    expect(value.Course_ID).toBe(1);
+});
+
+/**
+ * Ensures the DataController can find a list of courses.
+ */
+test('DataController Get Courses Test', async () => {
+    value = await DataController.getCourses(1, 1);
+    expect(value.courses.length).toBeGreaterThan(0);
 });
